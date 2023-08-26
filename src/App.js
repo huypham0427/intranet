c2d9317a49e3d80dc0269d31a0c1800422f71a25
huypham0427 <ptanhhuy27@yahoo.com>
2023-08-25 17:16:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import NewsFeed from './components/NewsFeed';
+import ProjectUpdates from './components/ProjectUpdates';
+import DocumentStorage from './components/DocumentStorage';
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ProductionDocs from './components/ProductionDocs';
+import CompanyDocs from './components/CompanyDocs';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <NavBar />
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/news">
+              <NewsFeed />
+            </Route>
+            <Route path="/project">
+              <ProjectUpdates />
+            </Route>
+            <Route path="/projects/productiondocs">
+              <ProductionDocs />
+            </Route>
+            <Route path="/projects/companydocs">
+              <CompanyDocs />
+            </Route>
+
+          </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
