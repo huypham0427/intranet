b1801c051099c60f6600b9f9442a6f886c83aa8e
huypham0427 <ptanhhuy27@yahoo.com>
2023-08-25 17:41:34 -0700

    test url2

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Header />
         <NavBar />
           <Switch>
-            <Route path="/intranet" exact component={HomePage} />
+            <Route path="https://huypham0427.github.io/intranet/" exact component={HomePage} />
             <Route path="/news">
               <NewsFeed />
             </Route>
@@ -28,7 +28,7 @@ function App() {
             <Route path="/projects/productiondocs">
               <ProductionDocs />
             </Route>
-            <Route path="/projects/companydocs">
+            <Route path="https://huypham0427.github.io/projects/companydocs">
               <CompanyDocs />
             </Route>
 
