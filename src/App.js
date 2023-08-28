558f10989b374712265c65eb1982abd6c0d7f2ec
huypham0427 <ptanhhuy27@yahoo.com>
2023-08-28 15:32:50 -0700

    using Routes in react-domv6

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProjectUpdates from './components/ProjectUpdates';
 import DocumentStorage from './components/DocumentStorage';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ProductionDocs from './components/ProductionDocs';
 import CompanyDocs from './components/CompanyDocs';
 
@@ -17,22 +17,13 @@ function App() {
       <div className="App">
         <Header />
         <NavBar />
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/news">
-              <NewsFeed />
-            </Route>
-            <Route path="/project">
-              <ProjectUpdates />
-            </Route>
-            <Route path="/projects/productiondocs">
-              <ProductionDocs />
-            </Route>
-            <Route path="/projects/companydocs">
-              <CompanyDocs />
-            </Route>
-
-          </Switch>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/news" element={<NewsFeed />} />
+          <Route path="/project" element={<ProjectUpdates />} />
+          <Route path="/projects/productiondocs" element={<ProductionDocs />} />
+          <Route path="/projects/companydocs" element={<CompanyDocs />} />
+        </Routes>
       </div>
     </Router>
     
